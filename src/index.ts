ff894374e5e6c6a3746788b49ee541f9d8d81dba
FrozenCell239 <val8ioks@gmail.com>
2025-03-05 14:56:59 +0100

    Security update - Added Helmet package configuration. - Allowed front-end origin with CORS package. - Added app auto-reloading on error. - Added some comments and minor changes.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import weatherRoutes from './routes/weatherRoutes';
 import moviesRoutes from './routes/moviesRoutes';
 
 // Config
-const app = express();
 const port = 2012;
-app.use(express.json());
+const app = express()
+    // Settings
+    .use(cors({origin: 'http://localhost:5173'}))
+    .use(express.json())
+    .use(helmet())
+    .use(helmet.contentSecurityPolicy())
+    .use(helmet.hidePoweredBy())
+    .use(express.urlencoded({ extended: true }))
 
-// Weather routes
-app.use('/weather', weatherRoutes);
-app.use('/movies', moviesRoutes);
+    // Routes
+    .use('/weather', weatherRoutes)
+    .use('/movies', moviesRoutes)
 
-// Starting the server
-app.listen(port, () => {console.log(`Server listening on port ${port}.`);});
+    // Starting the server
+    .listen(port, () => {console.log(`Server listening on port ${port}.`);})
+
+    // Auto-reloading on error
+    .on('error', error => {
+        console.error(error)
+        app.listen(port, () => console.log(`Server listening on port ${port}.`))
+    })
+;
